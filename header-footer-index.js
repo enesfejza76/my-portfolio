3c1ff070ae7369131791e27462b3a0b872f82069
CodeCrafter20 <133519757+CodeCrafter20@users.noreply.github.com>
2024-07-04 06:34:34 -1000

    new

---
+++ header-footer-index.js
@@ -0,0 +1,7 @@
+let headerLink = document.getElementById('headerLink');
+
+headerLink.innerHTML = "<li><a href='#work'>Project</a></li><li><a href='#about'>About</a></li><li><a href='#workExperience'>Work</a></li><li><a href='#skills'>Skills</a></li><li><a href='./links.html'>Links</a></li>";
+
+let footerLink = document.getElementById('footerLink');
+
+footerLink.innerHTML = "<li><a href='#work'>Project</a></li><li><a href='#about'>About</a></li><li><a href='#workExperience'>Work</a></li><li><a href='#skills'>Skills</a></li><li><a href='./links.html'>Links</a></li>";
---
+++ header-footer-link.js
@@ -0,0 +1,7 @@
+let headerLink = document.getElementById('headerLink');
+
+headerLink.innerHTML = "<li><a href='./index.html#work'>Project</a></li><li><a href='./index.html#about'>About</a></li><li><a href='./index.html#workExperience'>Work</a></li><li><a href='./index.html#skills'>Skills</a></li><li><a href='./links.html'>Links</a></li>";
+
+let footerLink = document.getElementById('footerLink');
+
+footerLink.innerHTML = "<li><a href='./index.html#work'>Project</a></li><li><a href='./index.html#about'>About</a></li><li><a href='./index.html#workExperience'>Work</a></li><li><a href='./index.html#skills'>Skills</a></li><li><a href='./links.html'>Links</a></li><li><a href='./links.html'>Get Started</a></li>";
